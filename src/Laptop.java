539e0f971e963f9f429ff34c82622b2f13720703
phinn88 <157661566+phinn88@users.noreply.github.com>
2024-03-21 07:35:21 +0700

    Update Laptop.java

--- src/Laptop.java
+++ src/Laptop.java
@@ -1,2 +1,16 @@
-public class Laptop {
+public class Laptop extends Computer {
+    int weight;
+
+    public Laptop(String name, String ID, float price, int weight) {
+        super(name, ID, price);
+        this.weight = weight;
+    }
+
+    @Override
+    void showInfo() {
+        System.out.println("Name = "+name);
+        System.out.println("Id = "+ID);
+        System.out.println("Price = "+price);
+        System.out.println("Weight = "+weight);
+    }
 }
