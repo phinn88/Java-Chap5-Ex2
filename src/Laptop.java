099cc558a9a4c9e04cda17b998a809e561d7c989
phinn88 <157661566+phinn88@users.noreply.github.com>
2024-03-21 07:36:05 +0700

    Update Laptop.java

--- src/Laptop.java
+++ src/Laptop.java
@@ -1,2 +1,24 @@
-public class Laptop {
+public class PC extends Computer {
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public PC(String name, String ID, float price, String description) {
+        super(name, ID, price);
+        this.description = description;
+    }
+
+    @Override
+    void showInfo() {
+        System.out.println("Name = "+name);
+        System.out.println("Id = "+ID);
+        System.out.println("Price = "+price);
+        System.out.println("Description = "+description);
+    }
 }
