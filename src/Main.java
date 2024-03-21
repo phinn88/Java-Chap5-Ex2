7f174c10e8e326f358f707ecdf2186fad579f612
phinn88 <157661566+phinn88@users.noreply.github.com>
2024-03-21 07:37:06 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
-        PC pc;
+        PC pc = new PC("HP123", "87", 815.99f, "Ultra Book");
         pc.showInfo();
-
-        Laptop laptop;
+        Laptop laptop = new Laptop("Macbook Air", "JK5354", 1299, 1800);
         laptop.showInfo();
-
     }
 }
